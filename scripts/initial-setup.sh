6228231ee5c5f84ba271b6a927100da14908a045
dlee-bitovi <doug@bitovi.com>
2025-02-26 12:50:48 -0600

    cleaned up server initialization scripts

--- scripts/initial-setup.sh
+++ scripts/initial-setup.sh
@@ -3,17 +3,19 @@
 masterimg="medline-ghost-demo-mysql-master-1"
 slaveimg="medline-ghost-demo-mysql-slave-1"
 
-# docker exec -it $masterimg  mysql -uroot -prootpassword -e "CREATE USER 'replicator'@'%' IDENTIFIED BY 'replicatorpassword'; GRANT REPLICATION SLAVE ON *.* TO 'replicator'@'%';"
+# 1. add replication user on master
+docker exec -it $masterimg  mysql -uroot -prootpassword -e "CREATE USER 'replicator'@'%' IDENTIFIED BY 'replicatorpassword'; GRANT REPLICATION SLAVE ON *.* TO 'replicator'@'%';" 2>&1>/dev/null
 
-# docker exec -it $masterimg mysql -uroot -prootpassword -e "SHOW MASTER STATUS\G"
-
+# 2. get File and Position values from master
 masterfile=$(docker exec -it $masterimg mysql -uroot -prootpassword -e "SHOW MASTER STATUS\G" | awk '/File/ {print $2}')
-
 masterposition=$(docker exec -it $masterimg mysql -uroot -prootpassword -e "SHOW MASTER STATUS\G" | awk '/Position/ {print $2}')
 
+# 3. configure slave for replication, with user from 1.
 docker exec -it $slaveimg mysql -uroot -prootpassword -e "CHANGE MASTER TO MASTER_HOST='$masterimg', MASTER_USER='replicator', MASTER_PASSWORD='replicatorpassword', MASTER_LOG_FILE='$masterfile', MASTER_LOG_POS=$masterposition;" 2>&1>/dev/null
 
+# 4. start the slave
 docker exec -it $slaveimg mysql -uroot -prootpassword -e "START SLAVE;" 2>&1>/dev/null
 
+# 5. print slave replication status
 docker exec -it $slaveimg mysql -uroot -prootpassword -e "SHOW SLAVE STATUS\G" | awk '/Slave_/ {print}'
 
